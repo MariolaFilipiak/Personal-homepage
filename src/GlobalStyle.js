dbb4d49c9e23ba6381384f0347b8eb687ebf1262
Your Name <you@example.com>
2023-01-25 08:55:14 +0100

    GlobalStyle created

---
+++ src/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+html {
+  box-sizing: border-box;
+}
+
+*, ::after, ::before {
+  box-sizing: inherit;
+}
+body{
+  max-width: 1216px;
+  margin: 0 auto;
+  background-color: rgb(251, 251, 254);
+}
+  `;
