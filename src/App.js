6b9c562dd0f9c1bb091f33d42f26cda503fbc5b6
Your Name <you@example.com>
2023-01-25 08:57:00 +0100

    app.js edit (removed system code)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Footer from './Footer';
+import Header from './Header';
+import NextSklills from './NextSkills';
+import Portfolio from './Portfolio';
+import Skills from './Skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Skills/>
+    <NextSklills/>
+    <Portfolio/>
+    <Footer/>
+    </>
   );
 }
 
