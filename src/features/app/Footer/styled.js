f7d368bce62751b9608ccbddda378ff2efd30917
Your Name <you@example.com>
2023-01-26 12:21:22 +0100

    Footer created

---
+++ src/features/app/Footer/styled.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+margin-top: 120px;
+`;
+export const Email = styled.a`
+font-size:32px;
+line-height: 39px;
+letter-spacing: 0.05em;
+color: #252525;
+font-weight: 900;
+text-decoration: none;
+margin-top:24px;
+margin-bottom: 24px;
+color: #252525;
+`;
+
+export const Paragraph = styled.p`
+font-weight: 400;
+font-size: 18px;
+line-height: 140%;
+letter-spacing: 0.05em;
+color: #252525;
+max-width: 670px;
+margin-bottom: 56px;
+`;
