9bb9c2d4a32ba32b8844d69825eb3a35b82e5952
Your Name <you@example.com>
2023-01-26 13:08:37 +0100

    add core folder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyle } from './GlobalStyle';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './core/app/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/saga.js
+++
@@ -1,8 +0,0 @@
-import {all} from "redux-saga/effects";
-import { personalHomePageSaga } from "./Portfolio/Repositories/personalHomePageSaga";
-
-export default function* saga() {
-    yield all([
-        personalHomePageSaga(),
-    ]);
-}
