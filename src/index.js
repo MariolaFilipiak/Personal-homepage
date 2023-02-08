4c421d32d73b98564904ffd59da1c4bf8f253665
Your Name <you@example.com>
2023-02-08 18:02:29 +0100

    Provider and store add

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { GlobalStyle } from './GlobalStyle';
-import { Provider } from 'react-redux';
-import store from './core/app/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { App } from "./features/App";
+import store from "./features/Portfolio/repoStore";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <GlobalStyle/>
+  <Provider store={store}>
     <App />
-    </Provider>
-  </React.StrictMode>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
