973d393e764152f2c57f5f9fcfaf30ecd2936830
Your Name <you@example.com>
2023-02-09 12:07:36 +0100

    change axios for fetch

--- src/features/getRepos.js
+++ src/features/getRepos.js
@@ -1,11 +1,7 @@
-import axios from "axios";
 
-  export const getRepos = async () => {
-    const response = await axios.get("https://api.github.com/users/mariolafilipiak/repos");
-    if (!response.ok) 
-    {
-        new Error(response.statusText);
-    }
-    
-    return await response.data;
-};
+const URL = "https://api.github.com/users/mariolafilipiak/repos";
+
+fetch(`${URL}`)
+.then(response => response.json())
+.then(URL => console.log(URL))
+ .catch(error => console.error("Something is bad !", error));
