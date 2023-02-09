f9ae46f7003fa9147245429c9a4e2ac107e9e676
Your Name <you@example.com>
2023-02-09 11:19:02 +0100

    small change

--- src/features/Portfolio/Loading/index.js
+++ src/features/Portfolio/Loading/index.js
@@ -2,7 +2,7 @@ import { StyledSpinner, Wrapper } from "./styled";
 
 export const Loading = () => (
   <Wrapper>
-    Please wait, projects are being loaded...
+    Please wait, projects are being loaded...<br/>
     <StyledSpinner />
   </Wrapper>
 );
--- src/features/Portfolio/Loading/styled.js
+++ src/features/Portfolio/Loading/styled.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 import { ReactComponent as loading } from "./loading.svg";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  text-align: center;
+`;
+
 export const StyledSpinner = styled(loading)`
   max-width: 50px;
+  text-align: center;
 `;
