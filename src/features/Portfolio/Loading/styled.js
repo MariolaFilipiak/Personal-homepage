44469cc000ea8c74b36ad3ededc086fed4f6018f
Your Name <you@example.com>
2023-02-11 11:36:25 +0100

    styled

--- src/features/Portfolio/Loading/styled.js
+++ src/features/Portfolio/Loading/styled.js
@@ -3,9 +3,15 @@ import { ReactComponent as loading } from "./loading.svg";
 
 export const Wrapper = styled.div`
   text-align: center;
+  margin-top: 88px;
+  font-size:20px;
+  display: grid;
+  justify-content: center;
 `;
 
 export const StyledSpinner = styled(loading)`
-  max-width: 50px;
-  text-align: center;
+  margin-top: 48px;
+  height: 160px;
+  width: 160px;
+
 `;
