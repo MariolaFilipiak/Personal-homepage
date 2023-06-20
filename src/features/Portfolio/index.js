474b9f20eb53afbd7cb9b237a9cb40012885b546
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-06-20 09:39:14 +0200

    changed Portfolio components removed content , status etc.

--- src/features/Portfolio/index.js
+++ src/features/Portfolio/index.js
@@ -1,33 +1,96 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Container, Icon, Text, Title } from "./styled";
-import { Content } from "./Content";
-import { ReactComponent as Github } from "./github.svg";
-import {
-  fetchRepos,
-  selectRepos,
-  selectReposStatus,
-} from "../Portfolio/repoSlice";
+import { Name, Wrapper, Tile, Images, Box } from "./styled";
+import MovieBrowser from "./images/MovieBrowser.png";
+import shop from "./images/shop.png";
+import ulotne from "./images/ulotne.png";
+import todolist from "./images/todolist.png";
+import { ButtonsWrapper, Button } from "../../common/Components/Buttons";
+import { ListItem, ListSkills } from "../../common/Skills/styled";
+import { Title } from "./Projects/styled";
 
-export const Portfolio = () => {
-  const dispatch = useDispatch();
-  const reposStatus = useSelector(selectReposStatus);
-  const repos = useSelector(selectRepos);
-
-  useEffect(() => {
-    dispatch(fetchRepos());
-  }, [dispatch]);
-
+export const Projects = () => {
   return (
     <>
-      <Container>
-        <Icon>
-          <Github />
-        </Icon>
-        <Title>Portfolio</Title>
-        <Text>My recent projects</Text>
-      </Container>
-      <Content status={reposStatus} repos={repos} />
+    <Wrapper>
+      <Box>
+        <Images src={MovieBrowser} />
+        <Tile>
+          <Name>Movie Browser</Name>
+          <ListSkills>
+            <ListItem>React</ListItem>
+            <ListItem>CSS</ListItem>
+            <ListItem>Styled-component</ListItem>
+            <ListItem>PublicApi</ListItem>
+            <ListItem>JavaScript</ListItem>
+            <ListItem>Redux</ListItem>
+            <ListItem>Redux-Saga</ListItem>
+            <ListItem>React-router-dom</ListItem>
+          </ListSkills>
+        </Tile>
+        <ButtonsWrapper>
+          <Button to={"/movie-browser"}>
+          Details
+          </Button>
+        </ButtonsWrapper>
+      </Box>
+      <Box>
+        <Images src={shop} />
+        <Tile>
+          <Name>The Second Life</Name>
+          <ListSkills>
+            <ListItem>React</ListItem>
+            <ListItem>CSS</ListItem>
+            <ListItem>Bootstrap</ListItem>
+            <ListItem>TypeScript</ListItem>
+            <ListItem>Redux</ListItem>
+            <ListItem>Redux-Saga</ListItem>
+            <ListItem>React-router-dom</ListItem>
+          </ListSkills>
+        </Tile>
+        <ButtonsWrapper>
+          <Button to={"/the-second-life"}>
+          Details
+          </Button>
+        </ButtonsWrapper>
+      </Box>
+      <Box>
+        <Images src={ulotne} />
+        <Tile>
+          <Name>Ulotne Chwile Filipiak Fotografia</Name>
+          <ListSkills>
+            <ListItem>React</ListItem>
+            <ListItem>CSS</ListItem>
+            <ListItem>Styled Component</ListItem>
+            <ListItem>JavaScript</ListItem>
+            <ListItem>React-router-dom</ListItem>
+          </ListSkills>
+        </Tile>
+        <ButtonsWrapper>
+          <Button to={"/ulotne-chwile"}>
+          Details
+          </Button>
+        </ButtonsWrapper>
+      </Box>
+      <Box>
+        <Images src={todolist} />
+        <Tile>
+          <Name>To do list</Name>
+          <ListSkills>
+            <ListItem>React</ListItem>
+            <ListItem>CSS</ListItem>
+            <ListItem>Styled Component</ListItem>
+            <ListItem>JavaScript</ListItem>
+            <ListItem>Redux</ListItem>
+            <ListItem>Redux-Saga</ListItem>
+            <ListItem>React-router-dom</ListItem>
+          </ListSkills>
+        </Tile>
+        <ButtonsWrapper>
+          <Button to={"/to-do-list"}>
+            Details
+          </Button>
+        </ButtonsWrapper>
+      </Box>
+    </Wrapper>
     </>
   );
 };
