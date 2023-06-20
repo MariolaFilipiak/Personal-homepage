205c4afa5c200c4d59e49bb8e796bfc51d842cbe
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-06-20 09:33:17 +0200

    Routes added

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import PersonalHomepage from "./PersonalHomepage";
+import { GlobalStyle } from "./GlobalStyle";
+import { Route, Routes } from "react-router-dom";
+import MovieBrowser from "./features/Portfolio/Projects/MovieBrowser";
+import TheSecondLife from "./features/Portfolio/Projects/TheSecondLife";
+import UlotneChwile from "./features/Portfolio/Projects/UlotneChwile";
+import ToDoList from "./features/Portfolio/Projects/ToDoList";
+import { ScrollToTop } from "./common/ScrollToTop";
+
+export const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<PersonalHomepage />} />
+        <Route path="/movie-browser" element={<MovieBrowser />} />
+        <Route path="/the-second-life" element={<TheSecondLife />} />
+        <Route path="/ulotne-chwile" element={<UlotneChwile />} />
+        <Route path="/to-do-list" element={<ToDoList />} />
+      </Routes>
+    </>
+  );
+};
