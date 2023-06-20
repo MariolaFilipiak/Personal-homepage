d0b966ffd8b6c20846af08404e08ee3878e786c8
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-06-20 13:50:10 +0200

    font size changed

--- src/common/Components/Title/styled.js
+++ src/common/Components/Title/styled.js
@@ -3,9 +3,8 @@ import styled from "styled-components";
 export const Name = styled.p`
   color: ${({ theme }) => theme.color.emperor};
   margin: 5px;
-  font-size: 8vw;
+  font-size: 7vw;
   text-transform: uppercase;
   font-weight: 700;
   text-align: left;
-  border-bottom: 1px solid  grey;
 `;
